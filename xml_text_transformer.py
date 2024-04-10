7710040ca1d1367a53d2db2dd100323a2fb46569
Mitalib2001 <166221352+Mitalib2001@users.noreply.github.com>
2024-04-10 14:29:10 +0530

    Update xml_text_transformer.py

--- xml_text_transformer.py
+++ xml_text_transformer.py
@@ -1,3 +1,97 @@
+# import xml.etree.ElementTree as ET
+# import pandas as pd
+# import logging
+# import tkinter as tk
+# from tkinter import filedialog
+# import argparse
+
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s - %(levelname)s - %(message)s',
+#                     filename='task.log')
+
+# def extract_data(input_file):
+#     tree = ET.parse(input_file)
+#     logging.info(f"Started parsing {input_file}")
+#     root = tree.getroot()
+
+#     data = []
+#     ns = {'autosar': 'http://autosar.org/schema/r4.0'}
+#     for containers in root.findall('.//autosar:CONTAINERS',ns):
+#         short_name_container = containers.find('.//autosar:SHORT-NAME', ns).text
+#         definition_ref_container = containers.find('.//autosar:DEFINITION-REF', ns).text
+#         data.append({'Tag':'Container','Short Name': short_name_container, 'Definition Ref': definition_ref_container})
+#         for sub_containers in containers.findall('.//autosar:SUB-CONTAINERS', ns):
+#             short_name = sub_containers.find('.//autosar:SHORT-NAME', ns).text
+#             definition_ref = sub_containers.find('.//autosar:DEFINITION-REF', ns).text
+#             data.append({'Tag':'Sub-container','Short Name': short_name, 'Definition Ref': definition_ref})
+#     logging.debug("Parsing completed")
+#     return pd.DataFrame(data)
+    
+# def transform_string(input_str):
+#     logging.info("String processing started")
+#     capitalize_next = True
+#     processed_output = ''
+
+#     for char in input_str:
+#         if char != ' ': 
+#             if capitalize_next:
+#                 processed_output += char.upper()
+#             else:
+#                 processed_output += char.lower()
+#             capitalize_next = not capitalize_next
+#         else:
+#             processed_output += char
+
+#     logging.info(f"Processing completed: {processed_output}")
+#     return processed_output
+
+# def save_data(data, output_file, processed_string):
+#     data.to_excel(output_file, index=False)
+#     logging.info(f"Excel file saved successfully at {output_file}")
+#     print("The modified string is: ", processed_string)
+#     logging.info("String displayed successfully")
+
+# def save_to_excel(data, output_file):
+#     data.to_excel(output_file,index = False)
+#     logging.info("Excel file saved successfully")
+
+# def gui():
+#     root = tk.Tk()
+#     root.withdraw()  # Hide the main window
+
+#     input_file = filedialog.askopenfilename(title="Select XML File")
+#     if not input_file:
+#         logging.error("No input file selected")
+#         return
+
+#     output_file = filedialog.asksaveasfilename(title="Save As", defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+#     if not output_file:
+#         logging.error("No output file selected")
+#         return
+
+#     data = extract_data(input_file)
+#     save_to_excel(data, output_file)
+
+# def cli(input_str, input_file, result_file):
+#     data = extract_data(input_file)
+#     processed_str = transform_string(input_str)
+#     save_data(data, result_file, processed_str)
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--input_str','-Is', help="Input string")
+#     parser.add_argument('--input_file','-If', help="Input file")
+#     parser.add_argument('--result_file', '-Of', help="Result file")
+#     args = parser.parse_args()
+
+#     if args.input_str and args.input_file and args.result_file:
+#         logging.debug("Command line input detected")
+#         cli(args.input_str, args.input_file, args.result_file)
+#     else:
+#         logging.debug("GUI input detected")
+#         gui()
+
+
 import xml.etree.ElementTree as ET
 import pandas as pd
 import logging
@@ -7,11 +101,12 @@ import argparse
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='task.log')
+                    filename='xml_text_transformer.log'
+                    )
 
-def extract_data(input_file):
-    tree = ET.parse(input_file)
-    logging.info(f"Started parsing {input_file}")
+def parse_xml(xml_input_file):
+    tree = ET.parse(xml_input_file)
+    logging.info(f"parsing started {xml_input_file}")
     root = tree.getroot()
 
     data = []
@@ -27,28 +122,29 @@ def extract_data(input_file):
     logging.debug("Parsing completed")
     return pd.DataFrame(data)
     
-def transform_string(input_str):
+def process_string(input_str):
     logging.info("String processing started")
     capitalize_next = True
-    processed_output = ''
+    transform_text = ''
 
     for char in input_str:
         if char != ' ': 
             if capitalize_next:
-                processed_output += char.upper()
+                transform_text += char.upper()
             else:
-                processed_output += char.lower()
+                transform_text += char.lower()
             capitalize_next = not capitalize_next
         else:
-            processed_output += char
+            transform_text += char
 
-    logging.info(f"Processing completed: {processed_output}")
-    return processed_output
+    logging.info(f"Processing completed: {transform_text}")
+    return transform_text
 
-def save_data(data, output_file, processed_string):
+def save_data(data, output_file, str):
     data.to_excel(output_file, index=False)
-    logging.info(f"Excel file saved successfully at {output_file}")
-    print("The modified string is: ", processed_string)
+    logging.info("Excel file saved successfully")
+ 
+    print("The modified string is: ",str)    
     logging.info("String displayed successfully")
 
 def save_to_excel(data, output_file):
@@ -59,8 +155,8 @@ def gui():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
 
-    input_file = filedialog.askopenfilename(title="Select XML File")
-    if not input_file:
+    xml_input_file = filedialog.askopenfilename(title="Select XML File")
+    if not xml_input_file:
         logging.error("No input file selected")
         return
 
@@ -69,24 +165,24 @@ def gui():
         logging.error("No output file selected")
         return
 
-    data = extract_data(input_file)
+    data = parse_xml(xml_input_file)
     save_to_excel(data, output_file)
 
-def cli(input_str, input_file, result_file):
-    data = extract_data(input_file)
-    processed_str = transform_string(input_str)
-    save_data(data, result_file, processed_str)
+def cli(input_str, xml_input_file, output_file):
+    data = parse_xml(xml_input_file)
+    str = process_string(input_str)
+    save_data(data, output_file, str)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_str','-Is', help="Input string")
-    parser.add_argument('--input_file','-If', help="Input file")
-    parser.add_argument('--result_file', '-Of', help="Result file")
+    parser.add_argument('-string_input','-Is')
+    parser.add_argument('-xml_file_input','-If')
+    parser.add_argument('-excel_file_output', '-Of')
     args = parser.parse_args()
 
-    if args.input_str and args.input_file and args.result_file:
-        logging.debug("Command line input detected")
-        cli(args.input_str, args.input_file, args.result_file)
+    if args.string_input and args.xml_file_input and args.excel_file_output :
+        logging.debug("Input through command line")
+        cli(args.string_input, args.xml_file_input, args.excel_file_output)
     else:
-        logging.debug("GUI input detected")
+        logging.debug("input through GUI")
         gui()
